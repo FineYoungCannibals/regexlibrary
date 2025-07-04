6eb6ec0966d3dfb05945af42101bc713077bf37f
Finleyh <finley.d.hammond@outlook.com>
2025-07-03 21:09:01 -0400

    replaced capture groups with non-caps

--- regexlib/patterns.py
+++ regexlib/patterns.py
@@ -1,11 +1,34 @@
 import re
 
-# Precompiled patterns
-USCAN_PHONE_REGEX = re.compile(r'(\+1[-.\s]?)?(\(?\d{3}\)?[-.\s]?){1}\d{3}[-.\s]?\d{4}')
-INTER_PHONE_REGEX = re.compile(r'\+[0-9\s\-().]{7,20}')
-SSN_REGEX = re.compile(r'\d{3}-\d{2}-\d{4}|\d{3}\s*\d{3}\s*\d{3}')
-DOB_REGEX = re.compile(r'\d{1,2}[/-]\d{1,2}[/-]\d{2,4}|\d{2,4}[/-]\d{1,2}[/-]\d{1,2}')
-IP_REGEX = re.compile(r'\d{1,3}(\.\d{1,3}){3}')
-CC_REGEX = re.compile(r'\s*\d{16}\s*')
-DL_REGEX = re.compile(r'[A-Z]{0,3}[0-9]+', re.IGNORECASE)
-EMAIL_REGEX = re.compile(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+')
+USCAN_PHONE_REGEX = re.compile(
+    r'(?:\+1[-.\s]?)?(?:\(?\d{3}\)?[-.\s]?){1}\d{3}[-.\s]?\d{4}'
+)
+
+INTER_PHONE_REGEX = re.compile(
+    r'\+[0-9\s\-\(\).]{7,20}'
+)
+
+SSN_REGEX = re.compile(
+    r'\d{3}-\d{2}-\d{4}|\d{3}\s*\d{3}\s*\d{3}'
+)
+
+DOB_REGEX = re.compile(
+    r'\d{1,2}[/-]\d{1,2}[/-]\d{2,4}|\d{2,4}[/-]\d{1,2}[/-]\d{1,2}'
+)
+
+IP_REGEX = re.compile(
+    r'\d{1,3}(?:\.\d{1,3}){3}'
+)
+
+CC_REGEX = re.compile(
+    r'\s*\d{16}\s*'
+)
+
+DL_REGEX = re.compile(
+    r'[A-Z]{0,3}[0-9]+',
+    re.IGNORECASE
+)
+
+EMAIL_REGEX = re.compile(
+    r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+'
+)
