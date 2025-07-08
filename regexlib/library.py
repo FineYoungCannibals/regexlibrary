fea6fcbbd9ae4b604e85e4735a7d66c288241ce6
Finleyh <finley.d.hammond@outlook.com>
2025-07-08 10:06:54 -0400

    added ipv6

--- regexlib/library.py
+++ regexlib/library.py
@@ -26,11 +26,16 @@ class RegexLibrary(Enum):
         "Matches dates of birth in MM/DD/YYYY, DD/MM/YYYY, or YYYY/MM/DD formats with slashes or dashes."
     )
 
-    IP = RegexPattern(
+    IPV4 = RegexPattern(
         r'\d{1,3}(?:\.\d{1,3}){3}',
         "Matches IPv4 addresses."
     )
 
+    IPV6 = RegexPattern(
+        r'^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))',
+        "Matches IPv6 addresses."
+    )
+    
     CC = RegexPattern(
         r'\s*\d{16}\s*',
         "Matches 16-digit credit card numbers, allowing optional surrounding whitespace."
